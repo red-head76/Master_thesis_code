49b34c30751536b9127e9a0d5f5dee9e979ee598
red-head76 <reddy.uni@t-online.de>
2021-06-01 21:31:29 +0200

    also r_value with errorbars now

--- Diagonalization_spin_chain/output.py
+++ Diagonalization_spin_chain/output.py
@@ -252,8 +252,9 @@ def plot_r_fig3(chain_length, J, B0, periodic_boundaries, samples):
             # Averaging over samples and states at the same time
             mean_r_values[i, j] = np.mean(r_values)
 
-        plt.plot(B0, mean_r_values[i], marker="o",
-                 linestyle="--", label=f"N={N}")
+        yerrors = 1 / np.sqrt(samples[i] * 2**chain_length[i])
+        plt.errorbar(B0, mean_r_values[i], yerr=yerrors, marker="o", capsize=5,
+                     linestyle="--", label=f"N={N}")
     plt.xlabel("Magnetic field amplitude B0")
     plt.ylabel("r-value")
     plt.legend()
