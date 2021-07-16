330e40f79580b8c8fd2395ca89ac724f848de957
red-head76 <reddy.uni@t-online.de>
2021-07-16 16:46:50 +0200

    "." error in path declaration fixed

--- MCTDH_utils/start_calcs.py
+++ MCTDH_utils/start_calcs.py
@@ -4,4 +4,4 @@ import os
 # sbatch --export=ALL,input=*your_input_file1*.inp -J *name_of_job1* start_job.sh
 for filenames in sys.argv[1:]:
     os.system(
-        f"sbatch --export=ALL,input=.input_files/{filenames}, -J {filenames} start_job.sh")
+        f"sbatch --export=ALL,input=input_files/{filenames}, -J {filenames} start_job.sh")
