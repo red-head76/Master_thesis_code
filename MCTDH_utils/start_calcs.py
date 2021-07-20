9506643de082e9b4e4d684d0393dffd653c092f5
red-head76 <reddy.uni@t-online.de>
2021-07-20 17:01:56 +0200

    now able to start all jobs at once

--- MCTDH_utils/start_calcs.py
+++ MCTDH_utils/start_calcs.py
@@ -2,6 +2,18 @@ import sys
 import os
 
 # sbatch --export=ALL,input=*your_input_file1*.inp -J *name_of_job1* start_job.sh
-for filenames in sys.argv[1:]:
-    os.system(
-        f"sbatch --export=ALL,input={filenames}, -J {filenames} start_job.sh")
+if len(sys.argv) == 1:
+    print("Submitting all files...")
+    entries = os.scandir("./input_files")
+    input_files = []
+    for entry in entries:
+        if entry.name[-4:] == ".inp":
+            input_files.append(entry.name)
+    for filename in input_files:
+        os.system(f"sbatch --export=ALL,input=input_files/{filename}, -J {filename} start_job.sh")
+
+
+else:
+    for filename in sys.argv[1:]:
+        print(f"submitting {filename}")
+        os.system(f"sbatch --export=ALL,input={filename}, -J {filename} start_job.sh")
