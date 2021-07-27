9e43eefb36703cc0b64cbc27f27c0edee06f17ea
red-head76 <reddy.uni@t-online.de>
2021-07-27 19:19:07 +0200

    plodding thingies

--- MCTDH_data/plot_entropy.py
+++ MCTDH_data/plot_entropy.py
@@ -3,17 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from read_output import read_output
 
-filenames = ["./run40"]  # ["./run20", "./run40", "./run80"]
+# ["./run20", "./run40", "./run80"]
+filenames = ["./20-5sp_20wf_delocalized", "./20-5sp_20wf_localized"]
 data = [read_output(filename + "/output") for filename in filenames]
-wfs = [40]
-for dataset, wf in zip(data, wfs):
+labels = ["delocalized", "localized"]
+for dataset, label in zip(data, labels):
     # weights of subroom 1
-    ws1 = dataset["weights"][:, 0, :]
+    ws1 = dataset["weights"][0]
     # cut out the zero elements
     # ugly, but cuts out the the Runtime warning caused by of 0 values in log
     entropy = -np.sum(ws1 * np.log(ws1, where=ws1 > 0,
                                    out=np.zeros(ws1.shape)), axis=1)
-    plt.plot(dataset["time"], entropy, label=f"{wf} Wavfunctions")
+    plt.plot(dataset["time"], entropy, label=label)
     plt.semilogx()
     plt.legend()
+plt.hlines(np.log(20), 0, 100)
 plt.show()
--- MCTDH_data/plot_occupation_imbalance.py
+++ MCTDH_data/plot_occupation_imbalance.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 from read_output import read_output
 from configparser import ConfigParser
 
-filenames = ["./localized_with_split", "./localized_without_split"]
+filenames = ["./20-5sp_20wf_delocalized", "./20-5sp_20wf_localized"]
 # ["./run20", "./run40", "./run80"]
-pdb.set_trace()
 data = [read_output(filename + "/output") for filename in filenames]
 
 
@@ -19,14 +18,17 @@ def read_config(config_file):
 configs = [read_config(filename + "/config") for filename in filenames]
 
 wfs = [40]
-descriptions = ["with split", "without split"]
+descriptions = ["delocalized", "localized"]
 for dataset, config,  desc in zip(data, configs, descriptions):
     # Sz
     exp_sig_z = dataset["exp_q"]
     chain_length = config.getint("System", "chain_length")
+    # cut central spin if its there
+    exp_sig_z = exp_sig_z[:, :chain_length]
     occ_imbalance = np.where(np.arange(chain_length) % 2, exp_sig_z, -exp_sig_z).sum(axis=1)
     plt.plot(dataset["time"], occ_imbalance, label=desc)
     plt.xlabel("Time")
     plt.ylabel("Occupation imbalance")
+    plt.legend()
     plt.semilogx()
 plt.show()
