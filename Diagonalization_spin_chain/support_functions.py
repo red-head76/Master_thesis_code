031819e50da240dc581c274c9796d17248b4f4b2
red-head76 <reddy.uni@t-online.de>
2021-08-03 12:30:24 +0200

    renaming option rho_a to calc_rho_a

--- Diagonalization_spin_chain/support_functions.py
+++ Diagonalization_spin_chain/support_functions.py
@@ -136,12 +136,12 @@ def partial_trace(rho, spins_a, calc_rho_a=True):
         rho (array (float) [dim, dim] or [t, dim, dim]): full density matrix with dimensions of
                             the full hamiltonian. With optional time dimension in front.
         spins_a (int): spins in subspace a.
-        rho_a (bool, default: True): determines whether the partial trace over b (if True, results
-                                     in rho_a) or over a (results in rho_b) should be calculated.
+        calc_rho_a (bool, default: True): determines whether the partial trace over b
+                    (if True, results in rho_a) or over a (results in rho_b) should be calculated.
 
     Returns:
         rho_a (array (float) [dim_a, dim_a] or [t, dim_a, dim_a]): the partial trace over b
-                (or the other way around if rho_a=False).
+                (or the other way around if calc_rho_a=False).
     """
     axis_offset = np.size(rho.shape) - 2
     dim = rho.shape[-1]
@@ -151,9 +151,9 @@ def partial_trace(rho, spins_a, calc_rho_a=True):
         rhojkjk = rho.reshape(-1, dim_a, dim_b, dim_a, dim_b)
     else:
         rhojkjk = rho.reshape(dim_a, dim_b, dim_a, dim_b)
-    # If rho_a is true, axis 1 and 3 (+offset) are taken, axis 0 and 2 otherwise
-    return np.diagonal(rhojkjk, axis1=rho_a+axis_offset,
-                       axis2=(2+rho_a+axis_offset)).sum(axis=2+axis_offset)
+    # If calc_rho_a is true, axis 1 and 3 (+offset) are taken, axis 0 and 2 otherwise
+    return np.diagonal(rhojkjk, axis1=calc_rho_a+axis_offset,
+                       axis2=(2+calc_rho_a+axis_offset)).sum(axis=2+axis_offset)
 
 
 def save_data(filename, data, config_file, anim=False, fps=10):
