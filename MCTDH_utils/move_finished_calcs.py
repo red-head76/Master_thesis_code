324da161148893f9a1570aec54a523950a20e011
red-head76 <reddy.uni@t-online.de>
2021-08-22 09:21:50 +0200

    now moving the finished files, instead of nonfinished

--- MCTDH_utils/move_finished_calcs.py
+++ MCTDH_utils/move_finished_calcs.py
@@ -3,9 +3,12 @@ import shutil
 
 for entry in os.scandir("./input_files/"):
     if entry.is_dir():
+        finished = True
         for subentry in os.scandir("./input_files/" + entry.name + "/"):
             if subentry.name == "stop":
-                shutil.move("./input_files/" + entry.name, "../data/")
-                shutil.move("./input_files/" + entry.name + ".inp", "../data/")
-                shutil.move("./input_files/" + entry.name + ".op", "../data/")
+                finished = False
                 break
+        if finished:
+            shutil.move("./input_files/" + entry.name, "../data/")
+            shutil.move("./input_files/" + entry.name + ".inp", "../data/")
+            shutil.move("./input_files/" + entry.name + ".op", "../data/")
