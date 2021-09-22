af0f72dcfef6486782661dceafc02717719e9f7c
red-head76 <reddy.uni@t-online.de>
2021-09-22 11:21:48 +0200

    fixxxed (?)

--- Diagonalization_spin_chain/scan_for_missing.py
+++ Diagonalization_spin_chain/scan_for_missing.py
@@ -4,10 +4,10 @@ from configparser import ConfigParser
 
 path = sys.argv[1]
 names = path.split('/')[-1]
-
+if names == '':                 # path ends with '/'
+    names = path.split('/')[-2]
 config_object = ConfigParser()
-config_object.read(f"{path}/{names}_config.ini")
-
+config_object.read(f"{path}/{names}.ini")
 samples = config_object.getint("Output", "samples")
 
 for i in range(samples):
