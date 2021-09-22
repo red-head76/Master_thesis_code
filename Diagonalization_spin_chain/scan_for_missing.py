ccd83088df484e71baac970b40a6116a145d5442
red-head76 <reddy.uni@t-online.de>
2021-09-22 11:10:23 +0200

    buggy fix

--- Diagonalization_spin_chain/scan_for_missing.py
+++ Diagonalization_spin_chain/scan_for_missing.py
@@ -3,12 +3,13 @@ import sys
 from configparser import ConfigParser
 
 path = sys.argv[1]
+names = path.split('/')[-1]
 
 config_object = ConfigParser()
-config_object.read(f"{path}/{path}_config.ini")
+config_object.read(f"{path}/{names}_config.ini")
 
 samples = config_object.getint("Output", "samples")
 
 for i in range(samples):
-    if not os.path.isfile(f"{path}/{path}_{i}.npz"):
-        print(f"{path}_{i}.npz")
+    if not os.path.isfile(f"{path}/{names}_{i}.npz"):
+        print(f"{names}_{i}.npz")
