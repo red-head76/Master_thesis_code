74e2c84d3e24616a3b30eebf15459800527afd9d
red-head76 <reddy.uni@t-online.de>
2022-01-12 17:48:08 +0100

    L also able to change within indi_configs

--- Diagonalization_spin_chain/create_indi_configs.py
+++ Diagonalization_spin_chain/create_indi_configs.py
@@ -2,10 +2,12 @@ from create_config import create_config
 
 config_object = create_config()
 
-Js = [0, 1]
-J_xys = [0, 1]
-Bs = [1, 7]
-As = [1, 4, 8]
+Ls = [8, 10, 12]
+Js = [0]
+J_xys = [0]
+Bs = [1]
+As = [1]
+
 outputtypes = ["half_chain_entropy", "occupation_imbalance"]
 
 # System setup
@@ -21,7 +23,7 @@ Constants["scaling"] = "sqrt"
 
 # Output option
 Output = config_object["Output"]
-Output["sample"] = "100"
+Output["samples"] = "2"
 Output["show"] = "False"
 Output["save_plot"] = "False"
 
@@ -32,20 +34,25 @@ Other["timeend"] = "1000"
 Other["timesteps"] = "300"
 Other["seed"] = "0"
 
-for J in Js:
-    for J_xy in J_xys:
-        for B in Bs:
-            for A in As:
-                for outputtype in outputtypes:
-                    Constants["J"] = str(J)
-                    Constants["J_xy"] = str(J_xy)
-                    Constants["B0"] = str(B)
-                    Constants["A"] = str(A)
-                    Output["outputtype"] = f"plot_{outputtype}"
-                    if outputtype == "half_chain_entropy":
-                        signature = f"hce_{J}{J_xy}{B}{A}"
-                    elif outputtype == "occupation_imbalance":
-                        signature = f"oi_{J}{J_xy}{B}{A}"
-                    Output["filename"] = f"./Plots/individual_mechanisms/{signature}"
-                    with open(f"./config_files/{signature}.ini", 'w') as conf:
-                        config_object.write(conf)
+for L in Ls:
+    for J in Js:
+        for J_xy in J_xys:
+            for B in Bs:
+                for A in As:
+                    for outputtype in outputtypes:
+                        System["chain_length"] = str(L)
+                        Constants["J"] = str(J)
+                        Constants["J_xy"] = str(J_xy)
+                        Constants["B0"] = str(B)
+                        Constants["A"] = str(A)
+                        Output["outputtype"] = f"plot_{outputtype}"
+                        if outputtype == "half_chain_entropy":
+                            signature = "hce"
+                        elif outputtype == "occupation_imbalance":
+                            signature = "oi"
+                        if len(Ls) <= 1:
+                            signature += f"_{L}"
+                        signature += f"_{J}{J_xy}{B}{A}"
+                        Output["filename"] = f"./Plots/individual_mechanisms/{signature}"
+                        with open(f"./config_files/{signature}.ini", 'w') as conf:
+                            config_object.write(conf)
